b100abb53ef5f533b84b62026873a9342d409895
Aleksandr <al.losev.k@gmail.com>
2019-10-13 19:32:50 +0300

    build

---
+++ build/precache-manifest.db8f2b0c2eb08a1588c819b7f23b94d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1557c40d09ca8c2ce1a889c9cc173cab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b60961984894b854d25f",
+    "url": "/static/css/main.2e3b8b26.chunk.css"
+  },
+  {
+    "revision": "ad098cddf54b459a342d",
+    "url": "/static/js/2.d1f61cbd.chunk.js"
+  },
+  {
+    "revision": "b60961984894b854d25f",
+    "url": "/static/js/main.08706d27.chunk.js"
+  },
+  {
+    "revision": "71aa53abd085ec3fbbd1",
+    "url": "/static/js/runtime-main.6ec817df.js"
+  }
+]);
